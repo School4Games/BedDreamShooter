2dfe68a1b6a5571297841abb3e7acc3a4d17b9c7
Max-Gottwald <max.gottwald@school4games.net>
2014-12-10 16:25:18 +0100

    Revert "Alpha Version MS03"
    This reverts commit 7cc71b09053f3e3c421cf849da7c3520884b05fa.

---
+++ Alpha/New Unity Project/Assets/Scripts/hide_unhide_Collider.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class hide_unhide_Collider : MonoBehaviour {
+	public KeyCode offKey = KeyCode.X;
+	public KeyCode onKey = KeyCode.Y;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(onKey)) {
+			// show
+			this.collider2D.enabled = true;
+		}
+		
+		if (Input.GetKeyDown(offKey)) {
+			// hide
+			this.collider2D.enabled = false;
+		}
+	
+	}
+}
