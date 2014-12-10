7cc71b09053f3e3c421cf849da7c3520884b05fa
Max-Gottwald <max.gottwald@school4games.net>
2014-12-10 16:23:21 +0100

    Alpha Version MS03
    Daran arbeite ich weiter.
    
    Max

---
+++ BedDreamShooter Prog/Versionen/Alpha/Assets/Scripts/GameController.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour
+{
+	public float timer;
+
+	public GameObject slimer;
+	public Vector3 spawnValues;
+	public float startdelay;
+	public float spawndelay;
+	
+	void Start ()
+	{
+		SpawnWaves ();
+
+		timer = startdelay;
+	}
+
+	void Update()
+	{
+		timer -= Time.deltaTime;
+		if (timer <= 0) 
+		{
+			timer = spawndelay;
+			SpawnWaves ();
+		}
+	}
+
+	void SpawnWaves ()
+	{
+		float randomvalue = Random.Range (-spawnValues.y, spawnValues.y);
+		Vector3 spawnPosition = new Vector3 (spawnValues.x, randomvalue, spawnValues.z);
+		Quaternion spawnRotation = Quaternion.identity;
+		Instantiate (slimer, spawnPosition, spawnRotation);
+	}
+
+
+}
+	
