7108d70ca566b7c08ed465da810e9ea2084d241b
Max-Gottwald <max.gottwald@school4games.net>
2015-01-07 16:01:29 +0100

    new Spawningsystem
    wir haben nun ein neues SpawnSystem

--- Alpha/Alpha/Assets/Scripts/GameController.cs
+++ Alpha/Alpha/Assets/Scripts/GameController.cs
@@ -3,38 +3,58 @@ using System.Collections;
 
 public class GameController : MonoBehaviour
 {
-	public float timer;
-
-	public GameObject slimer;
+	//GameObject enemey
+	public GameObject enemey;
+	//Spawn punkt
 	public Vector3 spawnValues;
-	public float startdelay;
-	public float spawndelay;
-	
+	//Wie viele enemys pro wave
+	public int enemeyCount;
+	//Zeit bis zum nächsten enemey
+	public float spawnWait;
+	//Zeit bis das erste mal ein Gegner kommt
+	public float startWait;
+	//Zeit bis zur nächsten wave
+	public float waveWait;
+
 	void Start ()
 	{
-		SpawnWaves ();
-
-		timer = startdelay;
+		StartCoroutine (SpawnWaves ());
 	}
 
 	void Update()
 	{
-		timer -= Time.deltaTime;
-		if (timer <= 0) 
+
+	}
+
+	IEnumerator SpawnWaves ()
+	{
+		yield return new WaitForSeconds (startWait);
+		while (true)
 		{
-			timer = spawndelay;
-			SpawnWaves ();
+			for (int i = 0; i < enemeyCount; i++)
+			{
+
+
+				float randomvalue = Random.Range (-spawnValues.y, spawnValues.y);
+				Vector3 spawnPosition = new Vector3 (spawnValues.x, randomvalue, spawnValues.z);
+				Quaternion spawnRotation = Quaternion.identity;
+				Instantiate (enemey, spawnPosition, spawnRotation);
+
+				yield return new WaitForSeconds (spawnWait);
+			}
+			yield return  new WaitForSeconds (waveWait);
+			if (spawnWait >= spawnWait)
+			{
+				spawnWait = spawnWait -0.4f;
+				if(spawnWait < 0)
+				{
+					spawnWait = 4;
+				}
+			}
+			/*if (enemeyCount >= enemeyCount)
+			{
+				enemeyCount = enemeyCount + 1;
+			}*/
 		}
 	}
-
-	void SpawnWaves ()
-	{
-		float randomvalue = Random.Range (-spawnValues.y, spawnValues.y);
-		Vector3 spawnPosition = new Vector3 (spawnValues.x, randomvalue, spawnValues.z);
-		Quaternion spawnRotation = Quaternion.identity;
-		Instantiate (slimer, spawnPosition, spawnRotation);
-	}
-
-
 }
-	
