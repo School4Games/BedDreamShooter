6040ac3c59decc9d32893658394786da8c76568a
Max-Gottwald <max.gottwald@school4games.net>
2015-01-16 19:50:03 +0100

    pausii
    bin dran

---
+++ Alpha/Alpha/Assets/Scripts/Pause_Menu.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class Pause_Menu : MonoBehaviour 
+{
+
+	public CanvasGroup Panel;
+
+	// Use this for initialization
+	void Start () 
+	{
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Input.GetKey ("p"))
+		{
+			//CanvasGroup = new CanvasGroup; 
+		}
+	}
+}
