59cba0ebc9a9c6962b511dee26c36c1e260dccd9
Max-Gottwald <max.gottwald@school4games.net>
2015-01-18 18:13:18 +0100

    pause
    pause funzt mit keyCode P, jedoch wird das menü noch ned angezeigt

---
+++ Alpha/Alpha/Assets/Scripts/Ingame_UI/inGamePause.cs
@@ -0,0 +1,54 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class inGamePause : MonoBehaviour 
+{
+	public float alpha;
+	public CanvasGroup pauseUI;
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine (PauseCoroutine());
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		/*if (Input.GetKeyDown (KeyCode.P))
+		{
+			CanvasGroup.alpha = new Alpha(yellowFace.color.r,yellowFace.color.g,yellowFace.color.b,1);
+		}
+		else
+		{
+			CanvasGroup.alpha = 0;
+		}*/
+	}
+
+	// Pause
+	IEnumerator PauseCoroutine()
+	{
+		while(true)
+		{
+
+			if (Input.GetKeyDown (KeyCode.P))
+			{
+				if (Time.timeScale == 0)
+				
+				{
+					Time.timeScale = 1;
+				
+				}
+				else
+				{
+					Time.timeScale = 0;
+				
+				}
+			}
+			
+				yield return null;
+		}
+	}
+}
