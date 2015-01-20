ee22ac1838ebd5f7609fc54a8175f531b2a245f4
Max-Gottwald <max.gottwald@school4games.net>
2015-01-20 13:20:16 +0100

    pause menu fast fertig
    tralalalalaa

--- Alpha/Alpha/Assets/Scripts/Ingame_UI/inGamePause.cs
+++ Alpha/Alpha/Assets/Scripts/Ingame_UI/inGamePause.cs
@@ -5,43 +5,55 @@ using System.Collections;
 public class inGamePause : MonoBehaviour 
 {
 	public bool breaker;
-	public Button pause;
+	public GameObject pause;
+	//public GameObject reStart;
+	//public Button pauseButton;
 	// Use this for initialization
-	void Start () 
+	public void Start () 
 	{
-		//StartCoroutine (PauseCoroutine());
+		StartCoroutine (PauseCoroutine());
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		if (Input.GetButtonDown ("pause"))
+		/*if (Input.GetButtonDown ("pause"))
 		{
 			StartCoroutine (PauseCoroutine());
-		}
+		}*/
 		
 	}
-
+	public void Breaker()
+		{
+			if (breaker == false)
+			{
+				breaker = true;
+			}
+			
+		}
 
 	// Pause
-	IEnumerator PauseCoroutine()
+	public IEnumerator PauseCoroutine()
 	{
 		while(true)
 		{
 
-			if (Input.GetKeyDown (KeyCode.P))
+			if (Input.GetKeyDown (KeyCode.P) || breaker)
 			{
 				//this.GameObject.SetActive(false);
-
+				pause.SetActive(true);
+				breaker = false;
 				if (Time.timeScale == 0)
 
 				
-				{
-					Time.timeScale = 1;
+					{
+						Time.timeScale = 1;
 
-				
-				}
+						pause.SetActive(false);
+
+					}
 				else
+				//if (reStart.)
 				{
 					Time.timeScale = 0;
 					//this.GameObject.SetActive(true);
